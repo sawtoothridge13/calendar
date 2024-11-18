fd707a74fb1f14ad626f71dab18f8ac0c8110c66
Doug Andrews 13 <dougandrews13@doug-andrews-computer-2.local>
2024-11-18 14:10:19 +0100

    Add EventList component

---
+++ src/components/EventList/EventList.jsx
@@ -0,0 +1,43 @@
+import './EventListComponent.css';
+import React from 'react';
+
+const EventListComponent = ({ events }) => {
+  return (
+    <div className="event-list">
+      <h2>Events</h2>
+      {events.length > 0 ? (
+        <ul>
+          {events.map((event, index) => {
+            const formattedDate = event.date.toLocaleDateString();
+            const formattedTime = event.date.toLocaleTimeString([], {
+              hour: '2-digit',
+              minute: '2-digit',
+              hour12: false, // 24-hour format
+            });
+
+            return (
+              <li key={index}>
+                <p>
+                  <strong>Date:</strong> {formattedDate}
+                </p>
+                <p>
+                  <strong>Time:</strong> {formattedTime}
+                </p>
+                <p>
+                  <strong>Title:</strong> {event.title}
+                </p>
+                <p>
+                  <strong>Description:</strong> {event.description}
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p>No events to display</p>
+      )}
+    </div>
+  );
+};
+
+export default EventListComponent;
