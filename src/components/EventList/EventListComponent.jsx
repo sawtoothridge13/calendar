d7361e9aac4e826704ff0528a09e544dd8a22bdb
Doug Andrews 13 <dougandrews13@doug-andrews-computer-2.local>
2024-11-21 15:09:06 +0100

    Add styling to event details page

--- src/components/EventList/EventListComponent.jsx
+++ src/components/EventList/EventListComponent.jsx
@@ -5,48 +5,46 @@ const EventListComponent = ({ events, handleDeleteEvent }) => {
   return (
     <div className="event-list">
       {events.length > 0 ? (
-        <ul>
-          {events.map((event, index) => {
-            const eventDate = new Date(event.date);
-            const formattedDate = eventDate.toLocaleDateString();
-            const formattedTime = eventDate.toLocaleTimeString([], {
-              hour: '2-digit',
-              minute: '2-digit',
-            });
+        events.map((event, index) => {
+          const eventDate = new Date(event.date);
+          const formattedDate = eventDate.toLocaleDateString();
+          const formattedTime = eventDate.toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit',
+          });
 
-            return (
-              <div key={index} className="event-item">
-                <li className="event-details">
-                  <p>
-                    <strong>Date:</strong> {formattedDate}
-                  </p>
-                  <p>
-                    <strong>Time:</strong> {formattedTime}
-                  </p>
-                  <p>
-                    <strong>Home Team:</strong> {event.homeTeam}
-                  </p>
-                  <p>
-                    <strong>Away Team:</strong> {event.awayTeam}
-                  </p>
-                  <p>
-                    <strong>Sport:</strong> {event.sport || 'N/A'}
-                  </p>
-                  <p>
-                    <strong>Description:</strong> {event.description}
-                  </p>
-                </li>
-                <button
-                  className="delete-button"
-                  onClick={() => handleDeleteEvent(index)}
-                  aria-label="Delete Event"
-                >
-                  <FaTrash />
-                </button>
+          return (
+            <div key={index} className="event-item">
+              <div className="event-details">
+                <p>
+                  <strong>Date:</strong> {formattedDate}
+                </p>
+                <p>
+                  <strong>Time:</strong> {formattedTime}
+                </p>
+                <p>
+                  <strong>Home Team:</strong> {event.homeTeam}
+                </p>
+                <p>
+                  <strong>Away Team:</strong> {event.awayTeam}
+                </p>
+                <p>
+                  <strong>Sport:</strong> {event.sport || 'N/A'}
+                </p>
+                <p>
+                  <strong>Description:</strong> {event.description}
+                </p>
               </div>
-            );
-          })}
-        </ul>
+              <button
+                className="delete-button"
+                onClick={() => handleDeleteEvent(index)}
+                aria-label="Delete Event"
+              >
+                <FaTrash />
+              </button>
+            </div>
+          );
+        })
       ) : (
         <p>No events to display</p>
       )}
